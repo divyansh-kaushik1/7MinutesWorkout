6ea606faf5fd23abf0478b261e8a739d462a8c5d
Divyansh Kaushik <divyansh.kaushik99@gmail.com>
2022-11-26 20:59:46 +0530

    first commit

---
+++ app/src/main/java/dk/android/a7minutesworkout/WorkOutApp.kt
@@ -0,0 +1,10 @@
+package dk.android.a7minutesworkout
+
+import android.app.Application
+// create the application class
+class WorkOutApp: Application() {
+
+    val db:HistoryDatabase by lazy {
+        HistoryDatabase.getInstance(this)
+    }
+}
